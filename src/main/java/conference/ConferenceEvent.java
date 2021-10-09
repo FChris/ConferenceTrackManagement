39dea7ff0bb279c938e1c82f02b562e9c884e9c8
Christoph Frädrich <chris@treebug.de>
2021-10-09 11:28:13 +0200

    Add toString to record classes

--- src/main/java/conference/ConferenceEvent.java
+++ src/main/java/conference/ConferenceEvent.java
@@ -2,4 +2,8 @@ package conference;
 
 public record ConferenceEvent(String title, int length) {
 
+    @Override
+    public String toString() {
+        return this.title;
+    }
 }
--- src/main/java/conference/Talk.java
+++ src/main/java/conference/Talk.java
@@ -2,7 +2,13 @@ package conference;
 
 public record Talk(String title, int length) {
 
-
+    @Override
+    public String toString() {
+        if (this.length <= 5) {
+            return this.title + " " + "lightning";
+        }
+        return this.title + " " + this.length + "min";
+    }
 }
 
 
