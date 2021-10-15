5a9807cc40e001be3a4dc12f6687e7a793f9cc3b
Christoph Frädrich <chris@treebug.de>
2021-10-15 07:28:17 +0200

    Add length check to Talk constructor

--- src/main/java/conference/Talk.java
+++ src/main/java/conference/Talk.java
@@ -2,6 +2,12 @@ package conference;
 
 public record Talk(String title, int length) {
 
+    public Talk {
+        if (length > 240) {
+            throw new IllegalArgumentException("Talk is too long. Maximum length for a Talk is 4 hours (240 Minutes)");
+        }
+    }
+
     @Override
     public String toString() {
         if (this.length <= 5) {
