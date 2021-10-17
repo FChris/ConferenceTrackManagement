b0b257de1aff163c08e8d5a552d1b88a7a6dc596
Christoph Frädrich <chris@treebug.de>
2021-10-17 15:37:50 +0200

    Add javadoc explaining the algorithm

--- src/main/java/conference/Conference.java
+++ src/main/java/conference/Conference.java
@@ -5,16 +5,54 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * This conference class builds a list of tracks from a given list of talks.
+ * <p>
+ * The conference class assigns talks to tracks and builds the list of tracks such that
+ * each talk finds a slot. The primary objective is to accommodate each talk while trying to fill each track
+ * as much as possible. There may be sessions which are not optimally filled, if the talk durations do not allow it.
+ * During this process talks are assigned start times when they are added to a session in a track.
+ * <p>
+ * The underlying problem of assigning talks to tracks in a conference is similar to the bin packing problem, where
+ * the sessions in the tracks represent bins and the talks represent the items to be packed.
+ * The chosen algorithm corresponds to the the First-fit-decreasing algorithm.
+ * The algorithm works as follows:
+ * <p>
+ *     <ol>
+ *     <li>Sort the list of talks in descending order (longest first)</li>
+ *     <li>Create an empty track</li>
+ *     <li>Place an item into the first track it fits
+ *      <ol>
+ *          <li>If the talk does not fit into any track, create a new track and add the talk to it</li>
+ *          <li>If a new track was created, add it to the list of tracks</li>
+ *      </ol>
+ *      </li>
+ *      <li>Repeat Step 2 until every item is assigned</li>
+ *      <li>Return the list of tracks</li>
+ *     </ol>
+ * </p>
+ */
 public class Conference {
 
     private List<Track> tracks;
 
+    /**
+     * Creates and initializes a conference object by assigning all talks to a track and saving the track to the
+     * track list.
+     *
+     * @param talks that will be assigned to tracks, list of talks is not expected to be sorted.
+     */
     public Conference(List<Talk> talks) {
         this.tracks = createTracksFromTalks(talks);
     }
 
+    /*
+     * Assign all talks to tracks. For algorithm description see the javadoc of this class
+     *
+     * @param talks - unsorted list with Talk objects that will be assigned to tracks
+     * @return list ot Track objects containing the talks
+     */
     private List<Track> createTracksFromTalks(List<Talk> talks) {
-        // sort talks by length so we can always start with the longest talk
         talks.sort(Comparator.comparingInt(Talk::getLength).reversed());
 
         ArrayList<Track> tracks = new ArrayList<>();
@@ -29,7 +67,7 @@ public class Conference {
         while (iter.hasNext()) {
             if (success) {
                 current = iter.next();
-            } else if (current != null){
+            } else if (current != null) {
                 Track newTrack = new Track();
                 newTrack.addTalk(current);
                 tracks.add(newTrack);
@@ -54,15 +92,16 @@ public class Conference {
         return tracks;
     }
 
+    @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < tracks.size(); i++) {
             builder.append("Track ");
-            builder.append(i+1);
+            builder.append(i + 1);
             builder.append(":\n");
             builder.append(tracks.get(i).toString());
 
-            if (i+1 < tracks.size()) {
+            if (i + 1 < tracks.size()) {
                 builder.append("\n\n");
             }
         }
--- src/main/java/conference/Track.java
+++ src/main/java/conference/Track.java
@@ -1,15 +1,35 @@
 package conference;
 
+/**
+ * A track is a pair of sessions which in turn contain talks.
+ *
+ * The track class is used to make sessions more manageable and represent the real life structure of the conference
+ * better. A track consists of a morning and an afternoon session.
+ */
 public class Track {
 
     private Session morningSession;
     private Session afternoonSession;
 
+    /**
+     * Initialize a Track with a pair of sessions.
+     */
     public Track() {
         this.morningSession = Session.createMorningSession();
         this.afternoonSession = Session.createAfternoonSession();
     }
 
+    /**
+     * Tries to add a talk to this session.
+     *
+     * First tries to add the talk to the morningSession and if unsuccessful tries to add it to the afternoon session.
+     * A talk can be added to a session if the current end time of a session + the length of the talk does not exceed
+     * the latest end time of a session.
+     *
+     * @param talk that should be added to this track
+     *
+     * @return true if talk was added to this track, false otherwise
+     */
     public boolean addTalk(Talk talk) {
         if (morningSession.addTalk(talk)) {
             return true;
